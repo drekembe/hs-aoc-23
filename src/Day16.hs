fa31ca35e6a0db02086dfaac9b999bcdc6750862
Miran Lipovaca <m.lipovaca@cargox.io>
2023-12-16 14:07:44 +0100

    Day 16

--- src/Day16.hs
+++ src/Day16.hs
@@ -2,33 +2,19 @@ module Day16 where
 
 import Data.Array
 import Control.Monad (guard)
-import Control.Applicative (empty)
+import qualified Data.Set as S
+import qualified Data.Map as M
 
 type Grid = Array (Int, Int) Char
-data Dir = U | D | L | R deriving (Show, Eq)
-data Pos = Pos (Int, Int) Dir deriving (Show, Eq)
-type History = [(Int, Int)]
-
-sample = unlines [
-  ".|...\\....",
-  "|.-.\\.....",
-  ".....|-...",
-  "........|.",
-  "..........",
-  ".........\\",
-  "..../.\\\\..",
-  ".-.-/..|..",
-  ".|....-|.\\",
-  "..//.|...."
-  ]
+data Dir = U | D | L | R deriving (Show, Eq, Ord)
+data Pos = Pos (Int, Int) Dir deriving (Show, Eq, Ord)
+type History = [Pos]
 
 parseInput :: String -> Grid
 parseInput input = array ((0, 0), (xe, ye)) (zip [(x, y) | y <- [0 .. ye], x <- [0 .. xe]] $ concat (lines input))
   where
     xe = (length . head . lines $ input) - 1
     ye = length (lines input) - 1
-
-initial = (Pos (-1,0) R, '.', [])
 
 down (Pos (x,y) _) = Pos (x,y+1) D
 up (Pos (x,y) _) = Pos (x,y-1) U
@@ -42,12 +28,23 @@ continue p@(Pos _ U) = up p
 inBounds arr (x,y) = (x >= xs) && (x <= xe) && (y >= ys) && (y <= ye)
   where ((xs,ys), (xe,ye)) = bounds arr
 
-move :: Grid -> Pos -> History -> [(Pos, History)]
-move grid (Pos (x,y) dir) history = do
-  let c = if inBounds grid (x,y) then grid ! (x,y) else '.'
-  (Pos (newX, newY) newDir) <- go c (Pos (x,y) dir)
-  guard $ inBounds grid (newX, newY)
-  return (Pos (newX, newY) newDir, (newX, newY):history)
+shootBeam :: Pos -> Grid -> Int
+shootBeam pos grid = shootBeam' (S.singleton pos) [pos]
+  where
+    shootBeam' :: S.Set Pos -> [Pos] -> Int
+    shootBeam' visited [] = length $ S.map (\(Pos (x,y) _) -> (x,y)) visited
+    shootBeam' visited beams = shootBeam' (visited `S.union` S.fromList nb) nb
+      where nb = concatMap (filter (`S.notMember` visited) . filter (inGrid grid) . (\p -> go (charAt p grid) p)) beams
+            charAt (Pos (x,y) dir) grid = grid ! (x,y)
+            inGrid grid (Pos p _) = inBounds grid p
+
+allStartingPositions :: Grid -> [Pos]
+allStartingPositions grid =
+  map (\x -> Pos (x,ys) D) [xs..xe] ++
+  map (\x -> Pos (x,ye) U) [xs..xe] ++
+  map (\y -> Pos (xs,y) R) [ys..ye] ++
+  map (\y -> Pos (xe,y) L) [ys..ye]
+  where ((xs,ys), (xe,ye)) = bounds grid
 
 
 go '.' p = [continue p]
@@ -69,6 +66,7 @@ go '|' p@(Pos _ U) = [continue p]
 go '|' p@(Pos _ D) = [continue p]
 go _ _ = error "unexpected go"
 
-getAnswerA = show . parseInput
+getAnswerA = show . shootBeam (Pos (0,0) R) . parseInput
 
-getAnswerB _ = "lmao"
+getAnswerB input = show . maximum . map (`shootBeam` grid)  . allStartingPositions $ grid
+  where grid = parseInput input
